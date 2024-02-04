f9f610a94f1f33875950881c45a33435da8bfae9
Amir Reza Tamandani <dadartm@gmail.com>
2024-02-04 10:09:18 +0330

    style/util: add clsx & tailwind-merge and centered all of the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          inter.className,
+          "flex flex-col items-center justify-center h-full pt-4"
+          //attention:
+          //this will center all of the project , remove cn and the style so you could
+          //keep the font and ditch this disaster
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
